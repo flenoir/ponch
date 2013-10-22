bf4bf4700a3b390679ff6c8b43fd70609ccf746c
Fabien Lenoir <flenoir@gmail.com>
2013-10-22 18:46:08 +0200

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ponch::Application.config.secret_key_base = '5dce7c6e165c94a9abc090c4145af397ce4fe8f1097e22174af6df9aaf63081e9b88a67cdf1f7ada466fcabeef839e128b4558a535c4d77a2722a4b6e2b7fa02'
